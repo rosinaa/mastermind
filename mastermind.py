9c9e027f475e55dc3b715c0a4a402b9c6eb258a1
Rosina <rosinaa@gmail.com>
2017-08-22 15:23:23 +0100

    add mastermind exception

--- mastermind.py
+++ mastermind.py
@@ -1,31 +1,35 @@
+from mastermind_exceptions import MastermindError
+
+
 def feedback(maker_code, breaker_code):
-	# black = 0
-	# white = 0
+	# check maker_code and breaker_code validity
+	if not isinstance(maker_code, list):
+		raise MastermindError('Invalid maker code')
+	if not isinstance(breaker_code, list):
+		raise MastermindError('Invalid breaker code')
+	if len(maker_code) != len(breaker_code):
+		raise MastermindError("Codes have different lengths")
+
 	feedback_pegs = []
 	maker_without_blacks = []
 	breaker_without_blacks = []
 
+	# check guess pegs in the right position and color
 	for original, guess in zip(maker_code, breaker_code):
 		if original == guess:
-			# black += 1
 			feedback_pegs.append(1)
 		else:
 			maker_without_blacks.append(original)
 			breaker_without_blacks.append(guess)
 
+    # check pegs with the right color and wrong position
 	for peg in breaker_without_blacks:
 		if peg in maker_without_blacks:
-			# white += 1
 			feedback_pegs.append(0)
 			maker_without_blacks.remove(peg)
 
-	# return (black, white)
 	return feedback_pegs
 
 print(feedback(['a', 'b', 'b', 'c'], ['b', 'b', 'a', 'c']))
 print(feedback(['a', 'b', 'c', 'd'], ['b', 'd', 'a', 'c']))
-print(feedback(['a', 'b', 'c', 'd'], ['b', 'd', 'a', 'c']))
-print(feedback(['a', 'b', 'c', 'd'], ['e', 'e', 'e', 'e']))
-
-
-
+print(feedback(['a'], ['e', 'e', 'e', 'e']))
